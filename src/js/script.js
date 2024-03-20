ed832af9717191614800c9f1bfc999e1c1a2662a
Steve <steve.gitprofile@outlook.com>
2024-03-20 11:13:42 +0200

    added game rules page, added button to naviagte pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Get player score elements
+const playerOneScoreEl = document.getElementById('score--0');
+const playerTwoScoreEl = document.getElementById('score--1');
+
+// get dice image
+const diceImageEl = document.querySelector('.dice');
+
+// hide image
+diceImageEl.classList.toggle('hidden');
+
+// function for a dice roll
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
